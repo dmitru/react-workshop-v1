f01fc0b0b1b02758277e5739a0a697dc1fc11a62
Dmitry Borody <dmitriy.borodiy@gmail.com>
2017-12-10 11:25:24 +0400

    State, setState, constructors, binding event handlers; controlled components

--- src/BookList.js
+++ src/BookList.js
@@ -5,11 +5,42 @@ import Book from './Book';
 import './BookList.css';
 
 export default class BookList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchInputValue: '',
+    };
+
+    this.handleSearchInputChange = this.handleSearchInputChange.bind(this);
+  }
+
+  handleSearchInputChange(event) {
+    this.setState(
+      {
+        searchInputValue: event.target.value,
+      },
+      () => {
+        console.log('async: ' + this.state.searchInputValue);
+      }
+    );
+    console.log('sync: ' + this.state.searchInputValue);
+  }
+
   render() {
     const { showSearch, books } = this.props;
+
+    console.log('render: ', this.state.searchInputValue);
+
     return (
       <div className="BookList">
-        {showSearch && <input className="SearchBooksInput" type="text" placeholder="Search books..." />}
+        {showSearch && (
+          <input
+            onChange={this.handleSearchInputChange}
+            className="SearchBooksInput"
+            type="text"
+            placeholder="Search books..."
+          />
+        )}
 
         {books.map(({ id, title, summary, isFavorite, reviewsCount }) => (
           <Book key={id} title={title} isFavorite={isFavorite} reviews={reviewsCount} />
