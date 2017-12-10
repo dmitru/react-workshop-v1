a527caea13fde973e119dd5e77c402c5bb8759cd
Dmitry Borody <dmitriy.borodiy@gmail.com>
2017-12-10 10:39:21 +0400

    Functional and class components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
 
+function Book() {
+  return (
+    <div className="Book">
+      <h1>Title</h1>
+    </div>
+  );
+}
+
+class BookList extends Component {
+  render() {
+    return (
+      <div className="BookList">
+        <Book />
+        <Book />
+      </div>
+    );
+  }
+}
+
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <div className="App">
+        <BookList />
+      </div>
+    );
   }
 }
 
